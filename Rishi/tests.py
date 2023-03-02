9015f0cf22bcc25eedd5ff7d89363fc9368bb264
Rishi More <rishimore102@gmail.com>
2023-03-02 22:02:54 +0530

    Updated tests

--- Rishi/tests.py
+++ Rishi/tests.py
@@ -11,12 +11,15 @@ params = {"N_FFT": 1024,
         "Mel_F_Min": 0,
         "Mel_F_Max": 8000,}
 
-path = 'Rishi\HelloHello5.wav'
-audio, mel = pattern_generate(path, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
+path_1 = 'Rishi\HelloHello5.wav'
+path_2 = 'Rishi\ThisIsRishi12345.wav'
+audio, mel_1 = pattern_generate(path_1, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
                                   params['Frame_Length'], params['Mel_F_Min'], params['Mel_F_Max'])
-mel = pad_crop(mel, mode = 'mean')
-print(mel.shape)
-plt.matshow(mel)
-plt.show()
-
-print(predict(mel,mel))
+audio, mel_2 = pattern_generate(path_1, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
+                                  params['Frame_Length'], params['Mel_F_Min'], params['Mel_F_Max'])
+mel_1 = pad_crop(mel_1, mode = 'mean')
+mel_2 = pad_crop(mel_2, mode = 'mean')
+print(mel_1.shape)
+# plt.matshow(mel)
+# plt.show()
+print(predict(mel_1,mel_2))
