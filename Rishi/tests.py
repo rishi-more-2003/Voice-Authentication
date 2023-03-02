3f1a16efff644a41bf593c0eb002e27e0ede2149
Rishi More <rishimore102@gmail.com>
2023-03-02 22:37:07 +0530

    Updated tests

--- Rishi/tests.py
+++ Rishi/tests.py
@@ -15,11 +15,11 @@ path_1 = 'Rishi\HelloHello5.wav'
 path_2 = 'Rishi\ThisIsRishi12345.wav'
 audio, mel_1 = pattern_generate(path_1, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
                                   params['Frame_Length'], params['Mel_F_Min'], params['Mel_F_Max'])
-audio, mel_2 = pattern_generate(path_1, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
+audio, mel_2 = pattern_generate(path_2, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
                                   params['Frame_Length'], params['Mel_F_Min'], params['Mel_F_Max'])
 mel_1 = pad_crop(mel_1, mode = 'mean')
 mel_2 = pad_crop(mel_2, mode = 'mean')
 print(mel_1.shape)
 # plt.matshow(mel)
 # plt.show()
-print(predict(mel_1,mel_2))
+print(predict(mel_2,mel_1))
