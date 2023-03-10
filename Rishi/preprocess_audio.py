a022f73eb74e5832658202dd3089cf8b390042a4
Rishi More <rishimore102@gmail.com>
2023-03-10 18:12:57 +0530

    Updated preprocess_audio.py

--- Rishi/preprocess_audio.py
+++ Rishi/preprocess_audio.py
@@ -142,9 +142,6 @@ def get_mel(path):
             
 if "__main__" == __name__:
     path = 'Rishi/audio_samples/HelloHello5.wav'
-    # audio, mel = pattern_generate(path, params['N_FFT'], params['Mel_Dim'], params['Sample_Rate'], params['Frame_Shift'],
-    #                               params['Frame_Length'], params['Mel_F_Min'], params['Mel_F_Max'])
-    # mel = pad_crop(mel, mode = 'mean')
     mel = get_mel(path)
     print(mel.shape)
     plt.matshow(mel)
